e8a640c1d012d91c8d37b1f4c9f6f79faa803d20
fiehra <switchedspace@gmail.com>
2024-05-17 16:36:07 +0200

    added scroll to top javascript

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let h1 = document.querySelector('h1');
 let button = document.getElementById('header-button');
+let scrollButton = document.getElementById('scrolltop');
 
 button.addEventListener('click', function() {
 	h1.textContent = 'finally learning javascript'
-	console.log('Button was clicked!');
+});
+
+scrollButton.addEventListener('click', function() {
+	window.scrollTo({top:0, behavior: 'smooth'});
 });
